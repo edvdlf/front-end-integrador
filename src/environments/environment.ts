a41c8f80b444dbc95c16de53e9d0ad3661285fbe
edvdlf <edvdlf@hotmail.com>
2025-12-30 15:31:28 -0300

    Melhorias identificadores para teste cypress

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: false,
   useMock: false,
   api: {
-    baseUrlAuth: 'https://integrador.vps6439.panel.icontainer.run',
-    baseUrl: 'https://integrador.vps6439.panel.icontainer.run/api',
-   //baseUrlAuth: 'http://192.168.0.106:8098',
-    //baseUrl: 'http://192.168.0.106:8098/api',
+    //baseUrlAuth: 'https://integrador.vps6439.panel.icontainer.run',
+    //baseUrl: 'https://integrador.vps6439.panel.icontainer.run/api',
+   baseUrlAuth: 'http://192.168.0.106:8098',
+    baseUrl: 'http://192.168.0.106:8098/api',
     //baseUrlAuth: 'http://localhost:8098',
     //baseUrl: 'http://localhost:8098/api',
     endpoints: {
